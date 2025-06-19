f56e04d11e5c92a9b1e732b40bbec1f594bab6f5
Tony Bui <z5448625@ad.unsw.edu.au>
2025-06-19 16:44:45 +1000

    server

---
+++ commbank-server/result.py
@@ -0,0 +1,21 @@
+
+# _id
+# 62a3f587102e921da1253d32
+# Name
+# "House Down Payment"
+# TargetAmount
+# 100000
+# TargetDate
+# 2025-12-09T00:00:00.000+00:00
+# Balance
+# 73501.82
+# Created
+# 2022-06-11T12:33:10.000+00:00
+# TransactionIds
+# null
+# TagIds
+# null
+# UserId
+# 62a29c15f4605c4c9fa7f306
+# Icon
+# "house emoji 🏠"
